36ae2d68366c819e1bd582ee8d8285c97daf6fee
Yanahi Demetri0 <yanahi@ciencias.unam.mx>
2026-02-17 18:36:10 -0600

    Practica 1

---
+++ Tablero.java
@@ -0,0 +1,18 @@
+/**
+ * Clase para manejar el tablero del juego
+ */
+public class Tablero {
+
+    // Tablero solución fijo
+    public static int[][] createSolution() {
+
+        return new int[][] {
+            {1,0,1,1,0},
+            {1,1,1,0,0},
+            {1,1,0,0,1},
+            {0,1,0,1,1},
+            {1,0,0,1,0}
+        };
+    }
+    //Agrega los métodos que creas que faltan en esta clase
+}
